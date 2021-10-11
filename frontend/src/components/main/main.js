94f0a2c5ebe234e3c9b195e255c9b7ce89855182
Vern Chao <vernchao91@gmail.com>
2021-10-11 12:17:31 -0700

    frontend auth started

---
+++ frontend/src/components/main/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>cutAbove</h1>
+        <footer>
+          Copyright &copy; 2021 cutAbove
+        </footer>
+      </div>
+    );
+  }
+}
