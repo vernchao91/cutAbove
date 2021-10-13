ecf5eb50bd2581df8a3e30d274dc313079534478
Vern Chao <vernchao91@gmail.com>
2021-10-13 15:18:37 -0700

    finished all crud routes

---
+++ frontend/src/reducers/entities/appointments_reducer.js
@@ -0,0 +1,28 @@
+import {
+  RECEIVE_APPOINTMENTS,
+  RECEIVE_APPOINTMENT,
+  REMOVE_APPOINTMENT,
+} from "../../actions/appointment_actions"
+
+const appointmentsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  let newState = Object.assign({}, oldState)
+
+  switch (action.type) {
+    case RECEIVE_APPOINTMENTS:
+      return Object.assign({}, oldState, action.appointments)
+
+    case RECEIVE_APPOINTMENT:
+      newState[action.appointment.id] = action.appointment
+      return newState
+
+    case REMOVE_APPOINTMENT:
+      delete newState[action.appointmentId]
+      return newState
+
+    default:
+      return oldState
+  }
+}
+
+export default appointmentsReducer
--- frontend/src/reducers/entities/entities_reducer.js
+++ frontend/src/reducers/entities/entities_reducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
-// import stylesReducer from "./styles_reducer";
+import stylesReducer from "./styles_reducer";
 import reviewsReducer from "./reviews_reducer";
-// import appointmentsReducer from "./appointments_reducer";
+import appointmentsReducer from "./appointments_reducer";
 // import messagesReducer from "./messages_reducer";
 
 export default combineReducers({
-  // styles: stylesReducer,
+  styles: stylesReducer,
   reviews: reviewsReducer,
-  // appointments: appointmentsReducer,
+  appointments: appointmentsReducer,
   // messages: messagesReducer,
 });
