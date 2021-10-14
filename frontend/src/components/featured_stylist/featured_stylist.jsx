2a74db9a468bd07aec10e0c07bf7ce482accb7f9
Hicham <>
2021-10-14 12:46:43 -0400

    home page stylist show button

--- frontend/src/components/featured_stylist/featured_stylist.jsx
+++ frontend/src/components/featured_stylist/featured_stylist.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class FeaturedStylist extends React.Component {
     render() {
@@ -6,6 +7,7 @@ class FeaturedStylist extends React.Component {
             <div className = "featured-stylist-container">
                 <div className = "profile-picture featured-stylist-picture">Profile picture goes here</div>
                 Image carousel
+                <Link to={'/stylists/61676f4e567f919e9e722d1a'}><button>View Details</button></Link>
                 <br/>
                 <div className = "featured-stylist-description">Martin has been cutting hair for 15 years in WhoVille.<br/>He gives such good skin fades, he makes European soccer players blush. Click here to read more about his story and see if he's available for an appointment</div>
             </div>
