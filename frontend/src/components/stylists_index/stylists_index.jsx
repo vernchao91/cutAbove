cd85b57738accf518b760186d51fdf36f1a36a96
Hicham <>
2021-10-17 23:32:24 -0400

    completed searchbar minus css

--- frontend/src/components/stylists_index/stylists_index.jsx
+++ frontend/src/components/stylists_index/stylists_index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import StylistIndexItem from './stylist_list_item'
+// import {useState} from 'react'
 
 class StylistsIndex extends React.Component {
-  // constructor(props){
-  //   super(props)
-
-  //   // this.renderIndex = this.renderIndex.bind(this)
-  // }
+  constructor(props){
+    super(props)
+    this.state = {search: ''}
+    // this.renderIndex = this.renderIndex.bind(this)
+    this.updateSearch = this.updateSearch.bind(this)
+  }
 
   componentDidMount() {
     this.props.fetchStylists()
@@ -17,7 +19,12 @@ class StylistsIndex extends React.Component {
     // this.props.stylists = Object.assign({})
   }
 
+  updateSearch(e) {
+    this.setState({search: e.target.value})
+  }
+
   render() {
+    const searchTerm = this.state.search
     // debugger
     if (!this.props.stylists) {
       return null
@@ -25,9 +32,15 @@ class StylistsIndex extends React.Component {
 
     return (
       <div className='stylists-index-container'>
+        <input className = "search-bar" type="text" placeholder="Search for a Stylist..." onChange={this.updateSearch}/>
         <ul className='stylist-list'>
           {
-            this.props.stylists.map( (stylist, i) => <StylistIndexItem key={i} stylist={stylist}/> )
+            this.props.stylists.filter((val) => {
+              if (searchTerm === '') {
+                return val
+              } else if (val.handle.toLowerCase().includes(searchTerm.toLowerCase()) || val.firstName.toLowerCase().includes(searchTerm.toLowerCase()))
+                return val
+            }).map( (stylist, i) => <StylistIndexItem key={i} stylist={stylist}/> )
           }
         </ul>
       </div>
