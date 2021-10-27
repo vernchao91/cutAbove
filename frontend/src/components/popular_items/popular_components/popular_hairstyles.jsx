722f3227f545743abf4bd6328f7277d41af53b1e
Vern Chao <vernchao91@gmail.com>
2021-10-26 18:41:30 -0700

    changed main for heroku push

--- frontend/src/components/popular_items/popular_components/popular_hairstyles.jsx
+++ frontend/src/components/popular_items/popular_components/popular_hairstyles.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-// import style1 from './hairstyles/1-trending-hairstyle-for-women-CNfIfNdDDYf.jpeg'
-// import style2 from './hairstyles/1-voluminous-lob-haircut-with-bangs-CLJg371BdpY.jpeg'
-// import style3 from './hairstyles/10-medium-multi-colored-hair-CPo1nN_Le5v.jpeg'
-// import style4 from './hairstyles/12-long-shaggy-bob-haircut-B56DTa0BthM.jpeg'
-// import style5 from './hairstyles/4-medium-hair-with-wispy-bangs-CO0BCGpjXwf.jpeg'
-// import style6 from './hairstyles/8-shag-haircut-with-bangs-B32dmpRlowA.jpeg'
+import style1 from './hairstyles/1-trending-hairstyle-for-women-CNfIfNdDDYf.jpeg'
+import style2 from './hairstyles/1-voluminous-lob-haircut-with-bangs-CLJg371BdpY.jpeg'
+import style3 from './hairstyles/10-medium-multi-colored-hair-CPo1nN_Le5v.jpeg'
+import style4 from './hairstyles/12-long-shaggy-bob-haircut-B56DTa0BthM.jpeg'
+import style5 from './hairstyles/4-medium-hair-with-wispy-bangs-CO0BCGpjXwf.jpeg'
+import style6 from './hairstyles/8-shag-haircut-with-bangs-B32dmpRlowA.jpeg'
 
 class PopularHairstyles extends React.Component {
     constructor(props) {
@@ -59,8 +59,8 @@ class PopularHairstyles extends React.Component {
         if (!this.props.styles) return null
         return (
             <div className = "style-item-container">
-                {this.renderSixRandomStyles()}
-               {/* <div className = "style-item">
+                {/* {this.renderSixRandomStyles()} */}
+               <div className = "style-item">
                 <div className = "style-item-picture">
                     <img src={style1} alt = "Hich whyyyyy"/>
                 </div>
@@ -95,7 +95,7 @@ class PopularHairstyles extends React.Component {
                     <img src={style6} alt = "fine I don't"/>
                 </div>
                 <div className = "style-item-name">Curtain Bangs<br/>and Shag Haircut</div>
-               </div> */}
+               </div>
             </div>
         )
     }
